0db8dbf79d40f519b1b082f5a19ac39605d2a3fa
Gabriel <gpawlewski@gmail.com>
2025-07-06 18:55:06 +0200

    Delete Anagram_Checker.py

--- Anagram_Checker.py
+++
@@ -1,9 +0,0 @@
-def are_anagrams(str1, str2):
-    return sorted(str1) == sorted(str2)
-
-print(are_anagrams("evil", "vile"))
-# Output: True
-
-print(are_anagrams("truck", "turck"))
-# Output: False
-
