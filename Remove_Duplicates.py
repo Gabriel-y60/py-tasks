8b8be83fa2d23326d247bcf1ebf9cee72d8cdf0d
Gabriel <gpawlewski@gmail.com>
2025-07-06 18:56:15 +0200

    Delete Remove_Duplicates.py

--- Remove_Duplicates.py
+++
@@ -1,9 +0,0 @@
-def remove_duplicates(lst):
-    result = []
-    for item in lst:
-        if item not in result:
-            result.append(item)
-    return result
-
-print(remove_duplicates([1,1,2,3,3,3,4,5,6,6,77,77,8]))
-# Output: [1, 2, 3, 4, 5, 6, 77, 8]
